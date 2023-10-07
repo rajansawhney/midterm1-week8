2236d9c261306aa9dd19ae31aaaa2067a7257f32
Rajan Sawhney <11162679+rajansawhney@users.noreply.github.com>
2023-10-07 09:40:56 -1000

    Initial commit

---
+++ sum_of_rows_n_cols.c
@@ -0,0 +1,26 @@
+/*
+II. Sum of rows and columns of a square matrix
+
+Write a C program that finds the sum of individual rows and individual columns of a square matrix.
+The size and elements of the matrix are provided by the user.
+
+Note:
+1. Input integers for size of square matrix and its elements
+2. Display elements and sums using the following printf format only to match example output:
+    printf("%4d", <element or sum>)
+
+
+Example:
+Input square matrix of size 3:
+   1   2   3
+   4   5   6
+   7   8   9
+
+Sum of individual rows and columns:
+   1   2   3   6
+   4   5   6  15
+   7   8   9  24
+  12  15  18
+*/
+
+
